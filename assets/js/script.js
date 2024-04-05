eec6e879ece548c9a6fd8da228aa6784893e87c0
Katriel Chiu <katriel_chiu@msn.com>
2024-04-05 14:23:41 -0700

    implemented HTML layout of the page with each section and where the display goes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-const city = "San Jose";
-const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+// const city = "San Jose";
+// const url = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
 
-fetch(url).then(function (response) {
-    return response.json();
-}).then(function (data) {
-    console.log("--------- First request with geolocation --------")
-    console.log(data);
+// fetch(url).then(function (response) {
+//     return response.json();
+// }).then(function (data) {
+//     console.log("--------- First request with geolocation --------")
+//     console.log(data);
 
-    const lat = data[0].lat;
-    const lon = data[0].lon;
+//     const lat = data[0].lat;
+//     const lon = data[0].lon;
 
-    console.log(`${lat}, ${lon}`);
+//     console.log(`${lat}, ${lon}`);
 
-    const url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+//     const url2 = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-    fetch(url2).then(function (response2) {
-        return response2.json();
-    }).then(function (data2) {
-        console.log("--------- Second request with forecast --------");
-        console.log(data2);
+//     fetch(url2).then(function (response2) {
+//         return response2.json();
+//     }).then(function (data2) {
+//         console.log("--------- Second request with forecast --------");
+//         console.log(data2);
 
-        for (let i = 0; i < data2.list.length; i++) {
-            console.log(data2.list[i].main.temp);
-        }
-    })
-})
+//         for (let i = 0; i < data2.list.length; i++) {
+//             console.log(data2.list[i].main.temp);
+//         }
+//     })
+// })
